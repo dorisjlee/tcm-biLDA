dfec1019c212240edd6f2e47a74434bc8f5cdf95
Edward Huang <ewhuang3@timan103.cs.illinois.edu>
2017-07-01 04:17:06 -0500

    Added command line argument for number of topics.

--- monolingual_lda_baseline.py
+++ monolingual_lda_baseline.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import datetime
+import argparse
+from multiprocessing import Pool
 import numpy as np
 import os
 import lda
@@ -11,28 +12,15 @@ import time
 ### original data). Writes out the herb counts, symptom counts, code list (for
 ### mapping symptoms/herbs to integers), and the word distributions for each
 ### topic. The number of topics will match the number of unique diseases.
-### Run time for 5000 iterations: 3.5 hours.
-
-date_format = '%Y-%m-%d'
+### Run time: 5 minutes.
 
 def generate_folders():
-    '''
-    Creates the results directory.
-    '''
-    res_dir = './results/'
-    if not os.path.exists(res_dir):
-        os.makedirs(res_dir)
-    count_dir = './data/count_dictionaries/'
-    if not os.path.exists(count_dir):
-        os.makedirs(count_dir)
-    code_list_dir = './data/code_lists/'
-    if not os.path.exists(code_list_dir):
-        os.makedirs(code_list_dir)
-    lda_res_dir = './results/lda_word_distributions/'
-    if not os.path.exists(lda_res_dir):
-        os.makedirs(lda_res_dir)
+    for folder in ('./results', './data/count_dictionaries',
+        './data/code_lists', './results/lda_word_distributions'):
+        if not os.path.exists(folder):
+            os.makedirs(folder)
 
-def get_patient_dct(filename):
+def get_patient_dct(run_num):
     '''
     Returns dictionary
     Key: (name, date of birth) -> (str, str)
@@ -40,37 +28,16 @@ def get_patient_dct(filename):
     containing the diseases, diagnosis dates, symptoms, and herbs of each visit.
     '''
     # Keep track of the unique set of diseases so we know n_topics.
-    patient_dct, disease_set = {}, set([])
+    patient_dct = {}
 
-    f = open(filename, 'r')
+    f = open('./data/train_test/train_no_expansion_%s.txt' % run_num, 'r')
     for i, line in enumerate(f):
-        diseases, name, dob, visit_date, symptoms, herbs = line.split('\t')
-        # Always ends with a colon, so the last element of the split will be
-        # the empty string.
-        disease_list = diseases.split(':')[:-1]
-        disease_set = disease_set.union(disease_list)
-
-        visit_date = visit_date.split('，')[1][:len('xxxx-xx-xx')]
-        # Format the diagnosis date.
-        visit_date = datetime.datetime.strptime(visit_date, date_format)
-
-        # Format symptom and herb lists and remove duplicates.
-        symptom_list = list(set(symptoms.split(':')[:-1]))
-        herb_list = list(set(herbs.split(':')[:-1]))
-
-        # Name, date of birth pair uniquely identifies a patient.
-        key = (name, dob)
-        # Initialize the patient's visit dictionary.
-        if key not in patient_dct:
-            patient_dct[key] = {}
-        # If multiple visits in one day, add on a second to the visit.
-        while visit_date in patient_dct[key]:
-            visit_date += datetime.timedelta(0,1)
-        # list(set()) removes duplicates.
-        patient_dct[key][visit_date] = (disease_list, symptom_list, herb_list)
+        diseases, name, dob, visit_date, symptoms, herbs = line.strip().split('\t')
+        symptom_list = symptoms.split(':')
+        herb_list = herbs.split(':')
+        patient_dct[(name, dob)] = (symptom_list, herb_list)
     f.close()
-
-    return patient_dct, disease_set
+    return patient_dct
 
 def get_symptom_and_herb_counts(patient_dct, run_num):
     '''
@@ -78,31 +45,29 @@ def get_symptom_and_herb_counts(patient_dct, run_num):
     patients with more than one visit. Writes the counts out to file.
     Returns the list of unique medical codes.
     '''
-    herb_count_dct, symptom_count_dct = {}, {}
+    symptom_count_dct, herb_count_dct = {}, {}
     for key in patient_dct:
-        visit_dct = patient_dct[key]
-        for date in visit_dct:
-            disease_list, symptom_list, herb_list = visit_dct[date]
+        symptom_list, herb_list = patient_dct[key]
 
-            # Update the counts of each symptom and herb.
-            for symptom in symptom_list:
-                if symptom not in symptom_count_dct:
-                    symptom_count_dct[symptom] = 0
-                symptom_count_dct[symptom] += 1
-            for herb in herb_list:
-                if herb not in herb_count_dct:
-                    herb_count_dct[herb] = 0
-                herb_count_dct[herb] += 1
+        # Update the counts of each symptom and herb.
+        for symptom in symptom_list:
+            if symptom not in symptom_count_dct:
+                symptom_count_dct[symptom] = 0
+            symptom_count_dct[symptom] += 1
+        for herb in herb_list:
+            if herb not in herb_count_dct:
+                herb_count_dct[herb] = 0
+            herb_count_dct[herb] += 1
+
+    dct_folder = './data/count_dictionaries'
 
     # Write out the unique symptoms and herbs to file.
-    herb_out = open('./data/count_dictionaries/herb_count_dct_%s.txt' %
-        run_num, 'w')
+    herb_out = open('%s/herb_count_dct_%s.txt' % (dct_folder, run_num), 'w')
     for herb in herb_count_dct:
         herb_out.write('%s\t%d\n' % (herb, herb_count_dct[herb]))
     herb_out.close()
 
-    symptom_out = open('./data/count_dictionaries/symptom_count_dct_%s.txt' %
-        run_num, 'w')
+    symptom_out = open('%s/symptom_count_dct_%s.txt' % (dct_folder, run_num), 'w')
     for symptom in symptom_count_dct:
         symptom_out.write('%s\t%d\n' % (symptom, symptom_count_dct[symptom]))
     symptom_out.close()
@@ -114,8 +79,7 @@ def write_code_list(code_list, run_num):
     Writes the code list out to file.
     '''
     out = open('./data/code_lists/code_list_%s.txt' % run_num, 'w')
-    for code in code_list:
-        out.write('%s\n' % code)
+    out.write('\n'.join(code_list))
     out.close()
 
 def get_matrix_from_dct(patient_dct, code_list):
@@ -124,42 +88,48 @@ def get_matrix_from_dct(patient_dct, code_list):
     '''
     patient_matrix = []
     for key in patient_dct:
-        visit_dct = patient_dct[key]
-        for date in sorted(visit_dct.keys()):
-            disease_list, symptom_list, herb_list = visit_dct[date]
-            curr_code_list = symptom_list + herb_list
-            # Create binary vectors for each patient visit.
-            curr_row = [1 if c in curr_code_list else 0 for c in code_list]
-            patient_matrix += [curr_row]
+        symptom_list, herb_list = patient_dct[key]
+
+        curr_code_list = symptom_list + herb_list
+        # Create binary vectors for each patient visit.
+        curr_row = [1 if c in curr_code_list else 0 for c in code_list]
+        patient_matrix += [curr_row]
     return np.array(patient_matrix)
 
-def run_baseline_lda(patient_matrix, code_list, disease_set):
-    # TODO: Just using 100 topics.
-    # model = lda.LDA(n_topics=len(disease_set), n_iter=5000, random_state=1)
-    model = lda.LDA(n_topics=100, n_iter=5000, random_state=1)
+def run_baseline_lda(patient_matrix):
+    model = lda.LDA(n_topics=args.num_topics, n_iter=5000, random_state=1)
     model.fit(patient_matrix)
     topic_word = model.topic_word_
     return topic_word
 
+def lda_pipeline(run_num):
+    patient_dct = get_patient_dct(run_num)
+
+    # code_list is the vocabulary list.
+    code_list = get_symptom_and_herb_counts(patient_dct, run_num)
+    write_code_list(code_list, run_num)
+
+    # Convert the patient dictionary to a matrix for LDA.
+    patient_matrix = get_matrix_from_dct(patient_dct, code_list)
+
+    # Run LDA.
+    topic_word = run_baseline_lda(patient_matrix)
+    np.savetxt('./results/lda_word_distributions/lda_word_distribution_%s_%s.txt' %
+        (args.num_topics, run_num), topic_word)
+
+def parse_args():
+    global args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--num_topics', required=True, type=int,
+        help='Number of monolingual LDA topics to train on.')
+    args = parser.parse_args()
+
 def main():
     generate_folders()
+    parse_args()
 
-    for run_num in range(10):
-        # Fetch the training patient record dictionary.
-        patient_fname = './data/train_test/train_no_expansion_%s.txt' % run_num
-        patient_dct, disease_set = get_patient_dct(patient_fname)
-
-        # code_list is the vocabulary list.
-        code_list = get_symptom_and_herb_counts(patient_dct, run_num)
-        write_code_list(code_list, run_num)
-
-        # Convert the patient dictionary to a matrix for LDA.
-        patient_matrix = get_matrix_from_dct(patient_dct, code_list)
-
-        # Run LDA.
-        topic_word = run_baseline_lda(patient_matrix, code_list, disease_set)
-        np.savetxt('./results/lda_word_distributions/lda_word_distribution_%s'
-            '.txt' % run_num, topic_word)
+    pool = Pool(processes=10)
+    pool.map(lda_pipeline, range(10))
 
 if __name__ == '__main__':
     start_time = time.time()
