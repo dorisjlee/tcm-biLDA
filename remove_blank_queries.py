0b8b08681e9c0616d021646c41533e58d173bfdc
Edward Huang <ewhuang3@timan103.cs.illinois.edu>
2017-07-01 19:47:10 -0500

    Changed stripping of last element for creating clean record list.

--- remove_blank_queries.py
+++ remove_blank_queries.py
@@ -15,17 +15,20 @@ def rewrite_input_file():
         if name == 'null' or dob == 'null' or visit_date == 'null':
             continue
 
-        disease_list = list(set(diseases.split(':')[:-1]))
-        symptom_list = list(set(symptoms.split(':')[:-1]))
-        herb_list = list(set(herbs.split(':')[:-1]))
+        # Remove duplicates from each attribute set.
+        disease_list = list(set(diseases.strip().split(':')[:-1]))
+        symptom_list = list(set(symptoms.strip().split(':')[:-1]))
+        herb_list = list(set(herbs.strip().split(':')[:-1]))
+
         # Skip empty labels, symptoms, or herbs.
         if 0 in (len(disease_list), len(symptom_list), len(herb_list)):
             continue
 
         # We exclude dosages. Dosages are in grams, so they have a 'G'.
         filtered_herb_list = [herb for herb in herb_list if 'G' not in herb]
-        visit_line = '%s\t%s\t%s\t%s\t%s\t%s:\n' % (diseases, name, dob,
-            visit_date, symptoms, ':'.join(filtered_herb_list))
+        visit_line = '%s\t%s\t%s\t%s\t%s\t%s\n' % (':'.join(disease_list),
+            name, dob, visit_date, ':'.join(symptom_list),
+            ':'.join(filtered_herb_list))
         # Check to make sure that this is indeed the first visit.
         converted_date = datetime.strptime(visit_date[7:visit_date.index('(')],
             '%Y-%m-%d')
@@ -37,6 +40,7 @@ def rewrite_input_file():
             key_line_dct[key] = visit_line
     f.close()
 
+    # Write out the first visits.
     out = open('./data/clean_HIS_tuple_word_first_visit.txt', 'w')
     for key in key_line_dct:
         out.write(key_line_dct[key])
